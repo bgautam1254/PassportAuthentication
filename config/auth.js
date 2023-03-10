866e23321ee3d8354c678d0d534f8ee549839545
bgautam1254 <bgautam1254@gmail.com>
2023-03-10 17:18:53 +0530

    first commit

---
+++ config/auth.js
@@ -0,0 +1,19 @@
+module.exports = {
+    ensureAuthenticated: (req, res, next) => {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        req.flash('error_msg', 'Please log in to view this resource');
+        res.redirect('/users/login');
+    }
+}
+
+// const ensureAuthenticated = (req, res, next) => {
+//     if (req.isAuthenticated()) {
+//         return next();
+//     }
+//     req.flash('error_msg', 'Please log in to view this resource');
+//     res.redirect('/users/login');
+// }
+
+// module.exports = ensureAuthenticated
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://bgautam1254:jOOnnodrBMKnogeq@cluster0.uxpscx2.mongodb.net/?retryWrites=true&w=majority'
+}
+
